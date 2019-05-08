107978dce19169f71f3962d6ef70da26c1a8f0bc
ilya395 <41100430+ilya395@users.noreply.github.com>
2019-05-08 15:31:50 +0300

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,27 +1,39 @@
 
 $(document).ready(function () {
 	var birdList = $('.bird-animation__item');
+	for (var i = 0; i < birdList.length; i++) {
+		$(birdList[i]).fadeTo( 0, 0, "linear" );
+	};
+	// махи крыльями
 	var birdFlies = setInterval(function () {
-			var v = 0;
-			var z = 0;
-			var A = 0;
-			var nextIter = 0;
-		for (var i = 0; i < birdList.length; i++) {
-			//birdList.css('opacity', '0');
-			console.log('начало '+nextIter, z);
-			if (nextIter == 0) {
-				v = 0;
-				z = v + 600;
-			} else if (nextIter != 0) {
-				v = nextIter;
-				z = nextIter + 600;
-				console.log('конец '+nextIter, z);
-			};
+		birdList.fadeTo( 0, 0, "linear" );
+		var v = 0;
+		var z = 0;
+		var dlitelnost = 300;
 
-			$(birdList[i]).delay(v).fadeIn(100).delay(z).fadeOut(100);
-			nextIter = v + z;
-			
+		for (var i = 0; i < birdList.length; i++) {
+
+			v = (i)*dlitelnost;
+			z = dlitelnost;
+
+			$(birdList[i]).delay(v).fadeTo( 0, 1, "linear" ).delay(z).fadeTo( 0, 0, "linear" );
+
+		console.log('включаем: '+v);
+		console.log('выключаем: '+z);
 		};
-		//birdList.removeClass('bird-flies');
-	}, 2400);
+
+	}, 1000);
+	// взлеты и падения
+	var birdUpAndDown = setInterval(function() {
+		$('.bird-animation').animate({
+			top: '-20px'
+		}, 1000, 'linear');
+		$('.bird-animation').animate({
+			top: '20px'
+		}, 1600, 'linear');
+		$('.bird-animation').animate({
+			top: '0px'
+		}, 1000, 'linear');
+	}, 3600);
+
 });
